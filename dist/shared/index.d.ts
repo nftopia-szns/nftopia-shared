5e8d02060744594a0d195a6b5d2e1491cf4dbfca
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-07-01 04:54:54 +0000

    try to fix: code suggestion

--- dist/shared/index.d.ts
+++ dist/shared/index.d.ts
@@ -1,4 +1,4 @@
-export * from "./platform";
-export * from "./network";
-export * from "./asset";
+export { MetaversePlatform } from "./platform";
+export { ChainId, EthereumChainId, BSCChainId, SolanaChainId, } from "./network";
+export { DecentralandAssetDto } from "./asset";
 //# sourceMappingURL=index.d.ts.map
--- shared/index.ts
+++ shared/index.ts
@@ -1,13 +1,15 @@
-// export {
-//     MetaversePlatform
-// } from "./platform"
-// export {
-//     ChainId,
-//     EthereumChainId,
-//     BSCChainId,
-//     SolanaChainId,
-// } from "./network"
+export {
+    MetaversePlatform
+} from "./platform"
+export {
+    ChainId,
+    EthereumChainId,
+    BSCChainId,
+    SolanaChainId,
+} from "./network"
+export {
+    DecentralandAssetDto
+} from "./asset"
+// export * from "./platform"
+// export * from "./network"
 // export * from "./asset"
-export * from "./platform"
-export * from "./network"
-export * from "./asset"
